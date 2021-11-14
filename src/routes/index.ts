45013a9c0c98634621f4fa3cae235966ed496bbb
jpecker <javier.pecker@mercadolibre.com>
2021-11-14 17:10:39 -0300

    subiendo mejoras

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import productsRouter from './productos';
 import cartRouter from './carrito';
+import userRouter from  './user';
+import authRouter from './auth';
+import { isLoggedIn } from '../middleware/admin';
 
 const router = Router();
 
 router.use('/products', productsRouter);
-router.use('/cart', cartRouter);
+router.use('/cart', isLoggedIn,cartRouter);
+router.use('/user', userRouter);
+router.use('/auth', authRouter)
 
 export default router;
